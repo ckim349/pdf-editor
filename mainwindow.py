c45361ef3c7e93e02720ceeca12ea17ed58b45b7
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-07-03 09:04:54 +1200

    added menu and toolbar

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QToolBar
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction, QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
@@ -7,9 +9,44 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Good Pdf Editor")
 
         menubar = self.menuBar()
-        file = menubar.addMenu("File")
-        quit_action = file.addAction("Quit")
+        file = menubar.addMenu("&File")
+        open_action = file.addAction("&Open")
+        file.addSeparator()
+        save_action = file.addAction("&Save as")
+        file.addSeparator()
+        quit_action = file.addAction("&Quit")
         quit_action.triggered.connect(self.quit)
+
+
+        edit = menubar.addMenu("&Edit")
+        undo_action = edit.addAction("&Undo")
+        redo_action = edit.addAction("&Redo")
+        edit.addSeparator()
+        cut_action = edit.addAction("&Cut")
+        copy_action = edit.addAction("&Copy")
+        paste_action = edit.addAction("&Paste")
+
+
+        view = menubar.addMenu("&View")
+
+
+        sign = menubar.addMenu("&Sign")
+
+
+        window = menubar.addMenu("&Window")
+
+
+        help = menubar.addMenu("&Help")
+
+        toolbar = QToolBar("Tool Bar for Tools")
+        toolbar.setIconSize(QSize(20, 20))
+        self.addToolBar(toolbar)
+
+        toolbar.addAction("&Save")
+        toolbar.addAction("&Print")
+        toolbar.addAction("&Find")
+
+
 
     def quit(self):
         self.app.quit()
