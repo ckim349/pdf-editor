8711e994fb36f8a630ee164ca889bed5d803cdf1
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-07-06 11:14:48 +1200

    Added pdf viewing functionality

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,8 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication, QTabWidget, QMainWindow, QToolBar, QWidget, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QPushButton
+from PySide6.QtPdf import QPdfDocument
+from PySide6.QtPdfWidgets import QPdfView
+from PySide6.QtCore import QSize
 from PySide6 import QtCore
 from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWebEngineCore import QWebEngineSettings
 from toolbar import ToolBar
 from menubar import MenuBar
 
@@ -11,6 +15,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.app = app
         self.setWindowTitle("Chulshin's Goodey Pdf Editor")
+        self.pdf_document = QPdfDocument(self)
 
         # Menubar
         menubar = MenuBar(self)
@@ -24,9 +29,6 @@ class MainWindow(QMainWindow):
         home_tab = QWidget()
         pdf_tab = QMainWindow()
 
-        pdf_tab_layout = QVBoxLayout()
-        pdf_tab.setLayout(pdf_tab_layout)
-
         tab_widget.addTab(home_tab, "Home")
         tab_widget.addTab(pdf_tab, "Pdf Editor")
 
@@ -36,6 +38,15 @@ class MainWindow(QMainWindow):
 
         main_layout = QVBoxLayout(central_widget)
         main_layout.addWidget(tab_widget)
+
+        # PDF Document
+        self.pdf_document.load("../samplepdfs/1.pdf")
+        pdf_view = QPdfView(pdf_tab)
+        pdf_view.setDocument(self.pdf_document)
+        pdf_view.setPageMode(QPdfView.PageMode.MultiPage)
+
+        pdf_tab.setLayout(QVBoxLayout())
+        pdf_tab.setCentralWidget(pdf_view)
 
         self.setLayout(main_layout)
 
